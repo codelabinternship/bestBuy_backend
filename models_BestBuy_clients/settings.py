293bdc3c7abd1e6caa859cc30811eb9e6aa9d512
Timur <htimur509@gmail.com>
2025-04-20 19:10:59 +0500

    Установил swagger

--- models_BestBuy_clients/settings.py
+++ models_BestBuy_clients/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'BestBuy_bot'
+    'BestBuy_bot',
+    'rest_framework',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
