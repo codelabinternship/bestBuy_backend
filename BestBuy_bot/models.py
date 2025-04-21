965e2526152c6f7a0f608b4a0d170fc5c3805529
imsolikha <sanmannasnam@gmail.com>
2025-04-21 15:41:44 +0500

    Category required is removeded

--- BestBuy_bot/models.py
+++ BestBuy_bot/models.py
@@ -279,7 +279,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     discount_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     stock_quantity = models.IntegerField(default=0)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', blank=True, null=True)
     brand = models.CharField(max_length=255, blank=True, null=True)
     media = models.ImageField(upload_to=product_media_upload_path, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
